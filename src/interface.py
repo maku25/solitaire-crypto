395d040f2afd61126b9ee19017302bf13965b6e2
maku25 <kut.kemal@icloud.com>
2025-04-13 20:00:14 +0200

    projet v2

--- src/interface.py
+++ src/interface.py
@@ -1,25 +1,25 @@
+import random
 from PyQt5.QtWidgets import (
     QMainWindow, QWidget, QVBoxLayout, QHBoxLayout,
     QLabel, QTextEdit, QPushButton
 )
-from solitaire import chiffrer_msg, dechiffrer_msg
+from solitaire import (
+    chiffrer_msg,
+    dechiffrer_msg
+)
 
 class Fenetre(QMainWindow):
     def __init__(self):
         super().__init__()
-        
         self.setWindowTitle("Mini-projet crypto")
-
+        
         widget_central = QWidget()
         self.setCentralWidget(widget_central)
-        
         layout_principal = QVBoxLayout()
         widget_central.setLayout(layout_principal)
         
         self.label_saisie = QLabel("Tapez votre message :")
         self.input_message = QTextEdit()
-        self.input_message.setPlaceholderText("Saisissez un message ici...")
-        
         layout_principal.addWidget(self.label_saisie)
         layout_principal.addWidget(self.input_message)
         
@@ -28,31 +28,39 @@ class Fenetre(QMainWindow):
         self.bouton_dechiffrer = QPushButton("Déchiffrer")
         layout_boutons.addWidget(self.bouton_chiffrer)
         layout_boutons.addWidget(self.bouton_dechiffrer)
-        
         layout_principal.addLayout(layout_boutons)
         
         self.label_resultat = QLabel("Résultat :")
         self.zone_resultat = QTextEdit()
         self.zone_resultat.setReadOnly(True)
-        
         layout_principal.addWidget(self.label_resultat)
         layout_principal.addWidget(self.zone_resultat)
+        
+        self.label_log = QLabel("Détails de l'algo :")
+        self.zone_log = QTextEdit()
+        self.zone_log.setReadOnly(True)
+        layout_principal.addWidget(self.label_log)
+        layout_principal.addWidget(self.zone_log)
         
         self.bouton_chiffrer.clicked.connect(self.action_chiffrer)
         self.bouton_dechiffrer.clicked.connect(self.action_dechiffrer)
         
-        self.jeu_initial = list(range(1, 55))
-    
+        cartes = list(range(1, 55))
+        random.shuffle(cartes)
+        self.jeu_initial = cartes
+
     def action_chiffrer(self):
+        self.zone_log.clear()
         message_clair = self.input_message.toPlainText()
-        jeu_pour_chiffrement = self.jeu_initial[:]
-        
-        texte_chiffre = chiffrer_msg(message_clair, jeu_pour_chiffrement)
-        self.zone_resultat.setText(texte_chiffre)
-    
+        deck_clone = self.jeu_initial[:] 
+        texte_chiffre, log = chiffrer_msg(message_clair, deck_clone)
+        self.zone_resultat.setHtml(f"<p><b>Résultat :</b> {texte_chiffre}</p>")
+        self.zone_log.setHtml(log)
+
     def action_dechiffrer(self):
+        self.zone_log.clear()
         message_chiffre = self.input_message.toPlainText()
-        jeu_pour_dechiffrement = self.jeu_initial[:]
-        
-        texte_dechiffre = dechiffrer_msg(message_chiffre, jeu_pour_dechiffrement)
-        self.zone_resultat.setText(texte_dechiffre)
+        deck_clone = self.jeu_initial[:] 
+        texte_dechiffré, log = dechiffrer_msg(message_chiffre, deck_clone)
+        self.zone_resultat.setHtml(f"<p><b>Résultat :</b> {texte_dechiffré}</p>")
+        self.zone_log.setHtml(log)
--- src/solitaire.py
+++ src/solitaire.py
@@ -1,10 +1,12 @@
-ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+#joker noir = 53 et rouge = 54
+
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 def lettre_en_nombre(x):
-    return ALPHABET.index(x.upper()) + 1
+    return alphabet.index(x.upper()) + 1
 
 def nombre_en_lettre(x):
-    return ALPHABET[x - 1]
+    return alphabet[x - 1]
 
 def deplacer_joker_noir(jeu):
     i = jeu.index(53)
@@ -23,80 +25,142 @@ def double_coupe(jeu):
     j2 = jeu.index(54)
     haut = min(j1, j2)
     bas = max(j1, j2)
-
     partie_haut = jeu[:haut]
     partie_milieu = jeu[haut:bas+1]
     partie_bas = jeu[bas+1:]
-
     jeu[:] = partie_bas + partie_milieu + partie_haut
 
 def coupe_par_derniere_carte(jeu):
     derniere = jeu[-1]
-    if derniere >= 53:
-        n = 53
-    else:
-        n = derniere
-
+    n = 53 if derniere >= 53 else derniere
     if n < 53:
         partie_sans_derniere = jeu[:-1]
         dessus = partie_sans_derniere[:n]
         reste = partie_sans_derniere[n:]
         jeu[:] = reste + dessus + [jeu[-1]]
 
+def formatter_deck(jeu):
+    formatted_cards = []
+    for card in jeu:
+        if card == 53:
+            formatted_cards.append("<span style='color: black; font-weight: bolder';>53</span>")
+        elif card == 54:
+            formatted_cards.append("<span style='color: red; font-weight: bolder';>54</span>")
+        else:
+            formatted_cards.append(f"<span style='color: gray'>{card}</span>")
+    return "[" + ", ".join(formatted_cards) + "]"
+
 def lire_cle(jeu):
+    log = []
     premiere = jeu[0]
+    log.append(f"<b>première carte [0]</b> {premiere}")
     if premiere >= 53:
-        return None
+        log.append("<span style='color: red;'>première carte = joker → clé invalide (None)</span>")
+        return None, log
 
     carte_lue = jeu[premiere]
+    log.append(f"<b>carte lue à l'index {premiere}:</b> {carte_lue}")
     if carte_lue >= 53:
-        return None
-    
+        log.append("<span style='color: red;'>carte lue = joker → clé invalide (None)</span>")
+        return None, log
+
     if carte_lue > 26:
-        carte_lue -= 26
-    
-    return carte_lue
+        nouvelle_valeur = carte_lue - 26
+        log.append(f"<b>carte lue ({carte_lue}) > 26:</b> donc %26 = {nouvelle_valeur}")
+        cle_finale = nouvelle_valeur
+    else:
+        log.append(f"carte lue ({carte_lue})")
+        cle_finale = carte_lue
+
+    log.append(f"<b>clé généré :</b> {cle_finale}")
+    return cle_finale, log
 
 def generer_cle_solitaire(jeu):
+    log = []
+    log.append(f"<b>paquet de carte initial</b> {formatter_deck(jeu)}")
+    
     deplacer_joker_noir(jeu)
+    log.append(f"<b>joker noir --1</b> {formatter_deck(jeu)}")
+    
     deplacer_joker_rouge(jeu)
+    log.append(f"<b>joker rouge --2</b> {formatter_deck(jeu)}")
+    
     double_coupe(jeu)
+    log.append(f"<b>double coupe</b> {formatter_deck(jeu)}")
+    
     coupe_par_derniere_carte(jeu)
-    return lire_cle(jeu)
+    log.append(f"<b>coupe simple</b> {formatter_deck(jeu)}")
+    
+    cle, log_calcul = lire_cle(jeu)
+    log.extend(log_calcul)
+    
+    return cle, log
 
-def generer_flux_cles(jeu, longueur):
+def generer_flux_cles_verbose(jeu, longueur):
     flux = []
-    while len(flux) < longueur:
-        cle = generer_cle_solitaire(jeu)
-        if cle is not None:
-            flux.append(cle)
-    return flux
+    full_log = []
+    deck_temp = jeu[:] 
+    cle, log = generer_cle_solitaire(deck_temp)
+    full_log.extend(log)
+    if cle is not None:
+        flux = [cle] * longueur
+    return flux, full_log
 
 def chiffrer_msg(message, jeu):
     message_maj = message.upper()
     lettres = [c for c in message_maj if 'A' <= c <= 'Z']
     msg_filtre = "".join(lettres)
     
-    cles = generer_flux_cles(jeu, len(msg_filtre))
+    cles, log_global = generer_flux_cles_verbose(jeu, len(msg_filtre))
+    
     resultat = []
+    log_detail = []
+    cle = cles[0] if cles else None
     for i, lettre in enumerate(msg_filtre):
         val_lettre = lettre_en_nombre(lettre)
-        val_cle = cles[i]
-        s = val_lettre + val_cle
-        if s > 26:
-            s -= 26
-        resultat.append(nombre_en_lettre(s))
-    return "".join(resultat)
+        calcul = f"{val_lettre} (<b>{lettre}</b>) + {cle} (clé)"
+        somme = val_lettre + cle
+        if somme > 26:
+            calcul += f" = {somme} → %26 {somme - 26}"
+            somme -= 26
+        else:
+            calcul += f" = {somme}"
+        lettre_result = nombre_en_lettre(somme)
+        calcul += f"=> <b>'{lettre_result}'</b>"
+        log_detail.append(f"<p style='margin:2px;'><span style='color: blue;'>Lettre {i+1} :</span> {calcul}</p>")
+        resultat.append(lettre_result)
+    
+    texte_chiffre = "".join(resultat)
+    log_global.append(f"<p><b>msg: <b> {msg_filtre}</p>")
+    log_global.append(f"<p><b>msg chiffré</b> {texte_chiffre}</p>")
+    full_log_combined = "<br>".join(log_global + log_detail)
+    
+    return texte_chiffre, full_log_combined
 
 def dechiffrer_msg(message_chiffre, jeu):
     msg_c = "".join(c for c in message_chiffre.upper() if 'A' <= c <= 'Z')
-    cles = generer_flux_cles(jeu, len(msg_c))
+    cles, log_global = generer_flux_cles_verbose(jeu, len(msg_c))
+    
     resultat = []
+    log_detail = []
+    cle = cles[0] if cles else None
     for i, lettre in enumerate(msg_c):
         val_chiffre = lettre_en_nombre(lettre)
-        val_cle = cles[i]
-        p = val_chiffre - val_cle
-        if p < 1:
-            p += 26
-        resultat.append(nombre_en_lettre(p))
-    return "".join(resultat)
+        calcul = f"{val_chiffre} (<b>'{lettre}'</b>) - {cle}"
+        diff = val_chiffre - cle
+        if diff < 1:
+            calcul += f" = {diff} → %26 {diff + 26}"
+            diff += 26
+        else:
+            calcul += f" = {diff}"
+        lettre_result = nombre_en_lettre(diff)
+        calcul += f"=> <b>'{lettre_result}'</b>"
+        log_detail.append(f"<p style='margin:2px;'><span style='color: blue;'>Lettre {i+1} :</span> {calcul}</p>")
+        resultat.append(lettre_result)
+    
+    texte_dechiffré = "".join(resultat)
+    log_global.append(f"<p><b>msg chiffré:</b> {msg_c}</p>")
+    log_global.append(f"<p><b>msg déchiffré:</b> {texte_dechiffré}</p>")
+    full_log_combined = "<br>".join(log_global + log_detail)
+    
+    return texte_dechiffré, full_log_combined
